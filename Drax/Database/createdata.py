b9511740974bd83533f84d3db152b9a655b6ca18
Fathi Saidani <129080095+ExpertkickTN@users.noreply.github.com>
2023-07-18 17:20:20 +0100

    Delete Drax directory

--- Drax/Database/createdata.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-connection = sqlite3.connect('AccountSystem.db')
-cur = connection.cursor()
-cur.execute("CREATE TABLE IF NOT EXISTS AccountDB(ID INTEGER PRIMARY KEY, FirstName TEXT, LastName TEXT, "
-            "Email TEXT, Password TEXT)")
-cur.execute("CREATE TABLE IF NOT EXISTS PASSFORGOT(ID INTEGER PRIMARY KEY, Email TEXT, NewPassword TEXT)")
-connection.commit()
-connection.close()
